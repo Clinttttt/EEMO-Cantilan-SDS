3821c5aea61f91b43f8af49a4a8b92745fc9ff33
Clint <clintvillanueva82@gmail.com>
2026-03-31 01:03:19 +0800

    Adding HanldeResponse

---
+++ EEMOCantilanSDS.Infrastructure/HttpClients/HandleResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEMOCantilanSDS.Infrastructure.HttpClients
+{
+    public class HandleResponse
+    {
+        private readonly HttpClient _http;
+        public HandleResponse(HttpClient http)
+        {
+            _http = http;   
+        }
+
+
+    }
+}
