90c7aae7a9182f1466dfae0381824872f75c796c
Clinttttt <clintvillanueva82@gmail.com>
2026-06-21 22:14:59 +0800

    chore: add Docker support for API and client

--- EEMOCantilanSDS.Client/Program.cs
+++ EEMOCantilanSDS.Client/Program.cs
@@ -11,9 +11,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
+
 
 app.UseStaticFiles();
 
